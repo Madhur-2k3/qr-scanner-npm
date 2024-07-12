1b96582bd24590679f2d42d560ae33849b7caff6
Sridhar-Konidela <sridharkonidela@outlook.com>
2024-07-12 22:49:20 +0530

    changed width to 800

--- src/components/Scanner.jsx
+++ src/components/Scanner.jsx
@@ -120,7 +120,7 @@ function Scanner() {
     </div> */}
     <video
         ref={videoRef}
-        width="480"
+        width="800"
         height="800"
         autoPlay
         className='h-72 mx-auto mt-8'
